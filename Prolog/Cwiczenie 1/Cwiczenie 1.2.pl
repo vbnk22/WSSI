eded57f70eaf58395cc6e23658abc633a00a3ce4
vbnk22 <127195140+vbnk22@users.noreply.github.com>
2024-04-08 23:12:14 +0200

    Update Cwiczenie 1.2.pl

--- Prolog/Cwiczenie 1/Cwiczenie 1.2.pl
+++ Prolog/Cwiczenie 1/Cwiczenie 1.2.pl
@@ -12,12 +12,14 @@ rodzic(hawier, ewelina).
 /*A*/
 rodzenstwo(Brat, Siostra) :-
     rodzic(Brat, Rodzic),
-    rodzic(Siostra, Rodzic).
+    rodzic(Siostra, Rodzic),
+    Brat \= Siostra.
 /*B*/
 kuzyni(Kuzyn1, Kuzyn2) :-
     rodzic(Kuzyn1, Rodzic1),
     rodzic(Kuzyn2, Rodzic2),               
-    rodzenstwo(Rodzic1, Rodzic2).
+    rodzenstwo(Rodzic1, Rodzic2),
+    Kuzyn1 \= Kuzyn2.
 /*C*/
 tesciowie(Dziadek, Babcia) :-
     rodzic(Dziecko, Matka),
@@ -25,18 +27,22 @@ tesciowie(Dziadek, Babcia) :-
     (
         (rodzic(Ojciec, Dziadek), rodzic(Matka, Babcia));
         (rodzic(Ojciec, Babcia), rodzic(Matka, Dziadek))
-    ).
+    ),
+    Dziadek \= Babcia.
 /*D*/
 rodzic_przyrodni(Dziecko, MatkaPrzyrodnia) :-
     rodzic(Dziecko, Rodzic),
     rodzic(Dziecko2, Rodzic),
-    rodzic(Dziecko2, MatkaPrzyrodnia).
+    rodzic(Dziecko2, MatkaPrzyrodnia),
+    Dziecko \= MatkaPrzyrodnia.
 /*E*/
 rodzenstwo_przyrodnie(Brat, Siostra) :-
     rodzic_przyrodni(Brat, Rodzic),
-    rodzic_przyrodni(Siostra, Rodzic).
+    rodzic_przyrodni(Siostra, Rodzic),
+    Brat \= Siostra.
 /*F*/
 szwagrowie(Szwagier1, Szwagier2) :-
     rodzic(Dziecko, Szwagier1),
     rodzic(Dziecko, Rodzic),
-    rodzenstwo(Rodzic, Szwagier2).
+    rodzenstwo(Rodzic, Szwagier2),
+    Szwagier1 \= Szwagier2.
