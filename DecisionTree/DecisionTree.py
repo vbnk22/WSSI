849934dbe9f1d20fa8cbc2d4d412ed17d1d2a1dc
vbnk22 <127195140+vbnk22@users.noreply.github.com>
2024-05-13 22:55:41 +0200

    Update DecisionTree.py

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,1 +1,119 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+
+    def fit(self, X, y):
+        if self.n_features is None:
+            self.n_features = int(np.sqrt(X.shape[1]))
+
+        self.root = self._grow_tree(X, y)
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_features, replace=False)
+        if not isinstance(feat_idxs, np.ndarray):
+            feat_idxs = np.array([feat_idxs])
+
+        best_feat, best_thresh = self._best_split(X, y, feat_idxs)
+
+        left_idxs, right_idxs = self._split(X[:, best_feat], best_thresh)
+
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+
+        return Node(best_feat, best_thresh, left, right)
+
+    def _best_split(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+        return split_idx, split_thresh
+
+    def _information_gain(self, y, X_column, threshold):
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        left_entropy = self._entropy(y[left_idxs])
+        right_entropy = self._entropy(y[right_idxs])
+
+        n = len(y)
+        left_weight = len(left_idxs) / n
+        right_weight = len(right_idxs) / n
+
+        children_entropy = left_weight * left_entropy + right_weight * right_entropy
+
+        return parent_entropy - children_entropy
+
+    def _split(self, X_column, split_thresh):
+        X_column = np.array(X_column)
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _most_common_label(self, y):
+        return np.bincount(y).argmax()
+
+data = datasets.load_iris()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+dtree = DecisionTree(max_depth=10)
+dtree.fit(X_train, y_train)
+predictions = dtree.predict(X_test)
+
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred) / len(y_test)
+
+acc = accuracy(y_test, predictions)
+print(acc)
